4207c40ed5c601a722b03512bd98b0ebbd5fd3db
nil <nil@ableton.com>
2018-09-10 16:01:28 +0200

    Error reporting

--- loadimg_win32.c
+++ loadimg_win32.c
@@ -1,18 +1,44 @@
-#include <wincodec.h>
-#include <synchapi.h>
 #include "stb_image_write.h"
+
+#include <wincodec.h>
+
+#include <synchapi.h>
+
 
 static IWICImagingFactory *wic_factory;
 static SRWLOCK wic_factory_lock = SRWLOCK_INIT;
 
-void split_images(char const *filename, char const* output_basename)
+// Use Windows Imaging Components to open images and split them
+// into as many subimages exist.
+// @url: https://docs.microsoft.com/en-us/windows/desktop/wic/-wic-about-windows-imaging-codec
+//
+// Standard formats included in Windows:
+// *GIF,
+// *TIFF,
+// BMP,
+// ICO,
+// JPEG,
+// PNG,
+// Windows Media Photo
+// Direct Draw Surface
+//
+// *GIF and TIFF support bundling multiple images in one filename.
+//
+
+// Kyocera scanner/printers generate TIFF images with multiple images when scanning
+// multiple pages
+//
+// @return false on failure, and fills error_desc_str with description
+bool split_images(char const *filename, char const* output_basename, char** error_desc_str)
 {
+    bool result = false;
     if (!wic_factory) {
-        AcquireSRWLockExclusive(&wic_factory_lock   );
+        AcquireSRWLockExclusive(&wic_factory_lock);
         if (!wic_factory) {
             CoInitializeEx(0, COINITBASE_MULTITHREADED);
             if (CoCreateInstance(&CLSID_WICImagingFactory, 0, CLSCTX_INPROC_SERVER, &IID_IWICImagingFactory, &wic_factory) < 0) {
                 ReleaseSRWLockExclusive(&wic_factory_lock);
+                bcatf_delim(error_desc_str, "Could not initialise Windows Imaging Components");
                 goto done;
             }
         }
@@ -23,6 +49,7 @@ void split_images(char const *filename, char const* output_basename)
     MultiByteToWideChar(CP_UTF8, 0, filename, -1, wide_filename, wide_filename_length);
     IWICBitmapDecoder *image_decoder = 0;
     if (wic_factory->lpVtbl->CreateDecoderFromFilename(wic_factory, wide_filename, 0, GENERIC_READ, WICDecodeMetadataCacheOnDemand, &image_decoder) < 0) {
+        bcatf_delim(error_desc_str, "Could not decode image");
         goto done;
     }
 
@@ -31,6 +58,7 @@ void split_images(char const *filename, char const* output_basename)
     {
         UINT y;
         if (image_decoder->lpVtbl->GetFrameCount(image_decoder, &y) < 0) {
+            bcatf_delim(error_desc_str, "Could not obtain frame count in image");
             goto done;
         }
         guard(y < INT_MAX);
@@ -39,26 +67,40 @@ void split_images(char const *filename, char const* output_basename)
     for (frame_index = 0; frame_index<num_frames; frame_index++) {
         IWICBitmapFrameDecode *image_frame = 0;
         IWICBitmapSource *rgba_image_frame = 0;
-        if (image_decoder->lpVtbl->GetFrame(image_decoder, frame_index, &image_frame) < 0) {
+        uint32_t width = 0;
+        uint32_t height = 0;
+        HRESULT frame_err = S_OK;
+        
+        frame_err = image_decoder->lpVtbl->GetFrame(image_decoder, frame_index, &image_frame);
+        if (frame_err == S_OK) {
+            frame_err = WICConvertBitmapSource(&GUID_WICPixelFormat32bppRGBA, image_frame, &rgba_image_frame);
+        }
+        if (frame_err == S_OK) {
+            frame_err = image_frame->lpVtbl->GetSize(image_frame, &width, &height);
+        }
+        if (frame_err != S_OK) {
+            bcatf_delim(error_desc_str, "Could not decode frame %d (other images have been dumped)", frame_index);
             goto done_with_frame;
         }
-        if (WICConvertBitmapSource(&GUID_WICPixelFormat32bppRGBA, image_frame, &rgba_image_frame) < 0) {
-            goto done_with_frame;
-        }
-        uint32_t width;
-        uint32_t height;
-        image_frame->lpVtbl->GetSize(image_frame, &width, &height);
+        
         uint32_t channels = 4;
         uint32_t buffer_size = channels * width * height;
         uint8_t *pixels = calloc(buffer_size, 1);
+        if (!pixels) {
+            bcatf_delim(error_desc_str, "Could not decode frame %d (out of memory)", frame_index);
+            goto done_with_frame;
+        }
         uint32_t buffer_stride = channels * width;
         if (rgba_image_frame->lpVtbl->CopyPixels(rgba_image_frame, 0, buffer_stride, buffer_size, pixels) < 0) {
+            bcatf_delim(error_desc_str, "Could not copy rgba pixels", frame_index);
             free(pixels);
             goto done_with_frame;
         }
-
         char *output_filename = aprintf("%s-%d.png", output_basename, frame_index);
-        stbi_write_png(output_filename, width, height, channels, pixels, buffer_stride);
+        if (!stbi_write_png(output_filename, width, height, channels, pixels, buffer_stride)) {
+            bcatf_delim(error_desc_str, "Could not write PNG to %s", output_filename);
+        }
+        free(output_filename);
         free(pixels);
 done_with_frame:
         if (image_frame) {
@@ -68,10 +110,12 @@ done_with_frame:
             rgba_image_frame->lpVtbl->Release(rgba_image_frame);
         }
     }
+    result = true;
 done:
     if (image_decoder) {
         image_decoder->lpVtbl->Release(image_decoder);
     }
+    return result;
 }
 
 #pragma comment(lib, "windowscodecs.lib")
--- split.c
+++ split.c
@@ -5,14 +5,17 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* aprintf(char const *fmt, ...);
 char* vaprintf(char const *fmt, va_list input_args);
-
+void bcatf_delim(char** output_str, char const *fmt, ...);
+void bfree(char **strbuf);
 void die(char const *fmt, ...);
+
 #define guard(expr) if (!(expr)) { exit(-2); }
 
-void split_images(char const *filename, char const* output_basename);
+bool split_images(char const *filename, char const* output_basename, char **error_buf);
 
 int main(int argc, char **argv)
 {
@@ -23,7 +26,11 @@ int main(int argc, char **argv)
     char *basename = argv[argi++];
     printf("input: %s\n", inputfile);
     printf("basename: %s\n", basename);
-    split_images(inputfile, basename);
+    char* error_desc = NULL;
+    if (!split_images(inputfile, basename, &error_desc)) {
+      printf("ERROR: %s\n", error_desc);
+      bfree(&error_desc);
+    }
     return 0;
 }
 
@@ -34,7 +41,7 @@ void die(char const *fmt, ...)
     char* buf = vaprintf(fmt, args);
     va_end(args);
     
-    printf("ERROR: %s", buf);
+    printf("ERROR: %s\n", buf);
     free(buf);
     exit(-1);
 }
@@ -64,6 +71,63 @@ char* aprintf(char const *fmt, ...)
     char *res = vaprintf(fmt, args);
     va_end(args);
     return res;
+}
+
+struct BufHeader
+{
+  int len;
+  char bytes[1];
+};
+
+void bfree(char **strbuf)
+{
+  struct BufHeader *hdr = *strbuf? (struct BufHeader*)(*strbuf - offsetof(struct BufHeader, bytes)) : 0;
+  free(hdr);
+  *strbuf = NULL;
+}
+
+void vbcatf(char **strbuf, char const *fmt, va_list input_args)
+{
+    va_list args;
+    va_copy(args, input_args);
+    int len = vsnprintf(NULL, 0, fmt, args);
+    guard(len >= 0);
+    va_end(args);
+
+    struct BufHeader *hdr = *strbuf? (struct BufHeader*)(*strbuf - offsetof(struct BufHeader, bytes)) : 0;
+    if (!hdr) {
+      hdr = calloc(sizeof(*hdr) + len + 1, 1);
+      guard(hdr);
+    } else {
+      hdr = realloc(hdr, sizeof(hdr) + hdr->len + len + 1);
+      guard(hdr);
+      memset(&hdr->bytes[hdr->len], 0, len + 1);
+    }
+    *strbuf = &hdr->bytes[0];
+    va_copy(args, input_args);
+    len = vsnprintf(&hdr->bytes[hdr->len], len + 1, fmt, args);
+    hdr->len += len>0? len:0;
+    va_end(args);
+}
+
+
+void bcatf(char **strbuf, char const* fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  vbcatf(strbuf, fmt, args);
+  va_end(args);
+}
+
+void bcatf_delim(char** output_str, char const *fmt, ...)
+{
+  if (*output_str) {
+    bcatf(output_str, ", ");
+  }
+  va_list args;
+  va_start(args, fmt);
+  vbcatf(output_str, fmt, args);
+  va_end(args);
 }
 
 #include "loadimg_win32.c"
